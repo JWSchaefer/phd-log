26b78d9b71d53309f5a155b2bbb9d525cabad9c8
JWSchaefer <joe.w.schaefer@gmail.com>
2024-10-03 19:33:58 +0100

    Initial commit

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,67 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Simple',
+    Svg: require('@site/static/img/simple.svg').default,
+    description: (
+      <>
+        Each entry to the log is a markdown file.
+      </>
+    ),
+  },
+  {
+    title: 'Focused',
+    Svg: require('@site/static/img/open.svg').default,
+    description: (
+      <>
+        Github actions allows me to write my entries without worrying about deployment. Just write, commit, forget.
+      </>
+    ),
+  },
+  {
+    title: 'Accessible',
+    Svg: require('@site/static/img/focus.svg').default,
+    description: (
+      <>
+       Github pages allows anyone to view my logs, anywhere.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
